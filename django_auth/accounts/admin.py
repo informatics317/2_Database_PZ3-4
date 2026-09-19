2876610b48a60f8b92b331a94b37cd07fddc439d
Каверина Дарья <dashkav210@gmail.com>
2026-09-19 22:09:05 +0300

    Add files via upload

---
+++ django_auth/accounts/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Course, Department, StudentProfile
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'department')
+    list_filter = ('department',)
+    search_fields = ('title',)
+
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'department')
+    list_filter = ('department',)
+    search_fields = ('user__username',)
+    filter_horizontal = ('courses',)
+
---
+++ django_auth/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path(
+        'profile/',
+        views.profile,
+        name='profile'
+    ),
+    path(
+        'courses/',
+        views.course_list,
+        name='course_list'
+    ),
+]
